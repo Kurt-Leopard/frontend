24c004402193aa1b7c90c04c5af35f621ac58ef4
Kurt Leopard <markangeloujamandre@gmail.com>
2024-05-09 23:39:47 +0800

    added commit

---
+++ src/api.js
@@ -0,0 +1,5 @@
+// const serverURL = `${window.location.protocol}//${window.location.host.replace(/:\d+$/, '')}`;
+// let API_URL = `${serverURL}:5000/`;
+let API_URL="https://backend-oqk1.onrender.com/";
+
+export {API_URL};
