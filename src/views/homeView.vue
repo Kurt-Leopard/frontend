e4672a9dddb410dd799bd180e439d0f72b8cc901
Kurt Leopard <markangeloujamandre@gmail.com>
2024-05-10 00:27:59 +0800

    redirect

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -151,8 +151,7 @@ onMounted(fetchData);
           </RouterLink>
           <RouterLink to="/views/profile" v-if="userProfile">
             <section class="flex items-center">
-              <p> <img :src="`https://hilarious-sfogliatella-c9ed00.netlify.app/src/assets/uploads/${userProfile}`"
-                  class=" rounded-full border   h-10 w-10" alt="">
+              <p> <img :src="`uploads/${userProfile}`" class=" rounded-full border   h-10 w-10" alt="">
               </p>
             </section>
           </RouterLink>
