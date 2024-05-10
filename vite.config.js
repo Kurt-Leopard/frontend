d0437f690a992da76d4c8a78f7bb33377e2fb176
Kurt Leopard <markangeloujamandre@gmail.com>
2024-05-10 22:24:49 +0800

    spotify upt

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +7,24 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'Spotify API',
+        short_name: 'Spotify API',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/spotify.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
